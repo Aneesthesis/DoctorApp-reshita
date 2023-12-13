53cc781fa0b6c28d09cb3758ae72d88328e02410
aneesthesis <trulyanees@gmail.com>
2023-12-13 18:53:06 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -11,32 +11,41 @@ import { Store } from "./store";
 import AddPatient from "./pages/AddPatient";
 import ViewPatients from "./pages/ViewPatients";
 import PatientProfile from "./pages/PatientProfile";
+import Footer from "./components/UI/Footer";
 
 function App() {
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
 
   return (
-    <BrowserRouter>
-      <ToastContainer
-        className="fixed top-0 left-[37%] transform -translate-x-1/2 z-50 md px-4"
-        limit={1}
-      />
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route
-          path={`/doctor/doctor-profile`}
-          element={<DoctorProfile />}
-        ></Route>
-        <Route path="/doctor/add-patient" element={<AddPatient />}></Route>
-        <Route path="/doctor/patients" element={<ViewPatients />}></Route>
-        <Route path="/doctor/patients/:id" element={<PatientProfile />}></Route>
+    <>
+      <div className="min-h-screen">
+        <BrowserRouter>
+          <ToastContainer
+            className="fixed top-0 left-[37%] transform -translate-x-1/2 z-50 md px-4"
+            limit={1}
+          />
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/signin" element={<Signin />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route
+              path={`/doctor/doctor-profile`}
+              element={<DoctorProfile />}
+            ></Route>
+            <Route path="/doctor/add-patient" element={<AddPatient />}></Route>
+            <Route path="/doctor/patients" element={<ViewPatients />}></Route>
+            <Route
+              path="/doctor/patients/:id"
+              element={<PatientProfile />}
+            ></Route>
 
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
+    </>
   );
 }
 
